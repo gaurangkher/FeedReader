a3152b05d3bd60bcc912311b3b8b078ded0496f8
gaurang <gaurangkher@yahoo.com>
2015-07-27 21:20:37 -0500

    give distinct ranks for articles

--- crude_ranking.pl
+++ crude_ranking.pl
@@ -3,6 +3,7 @@ use Moose;
 use Carp;
 use DBI;
 use DBD::Pg;
+use List::Util qw(shuffle);
 use LWP::Simple;
 use Log::Log4perl qw(:easy);
 use Data::Dumper;
@@ -20,37 +21,44 @@ my $dbh = DBI->connect(
 my $sth = $dbh->prepare("select id from source");
 $sth->execute;
 my @sources;
+my @array;
+my $i = 1;
 while (my @arr = $sth->fetchrow_array()) {
     push @sources, $arr[0];
+    push @array, $i;
+    $i++;
 }
 
+@array = shuffle @array;
+my $total = scalar @array;
 for my $source (@sources) {
     INFO qq{Ranking for source $source};    
     my $sth1 = $dbh->prepare(
-        "select id from article where date > '$date' and category_id not in (3,4,5,6,62,115,117) and source_id = $source order by date desc"
+        "select id from article where date >= '$date' and category_id not in (3,4,5,6,62,115,117) and source_id = $source order by date desc"
     );
+    my $num = shift @array;
     $sth1->execute;
     my $rank = 1;
     while (my @arr = $sth1->fetchrow_array()) {
         my ($id) = @arr;
-        
+        my $new_rank = $rank + $num;
         my $sth_new = $dbh->prepare("select id from ranking where id = '$id' "); 
         $sth_new->execute;
         my ($exist) = $sth_new->fetchrow_array();
 
         if ($exist) {
-            my $sth_u = $dbh->prepare("UPDATE ranking set page_rank = $rank where id = '$id'");
+            my $sth_u = $dbh->prepare("UPDATE ranking set page_rank = $new_rank where id = '$id'");
             $sth_u->execute;
             INFO q{UPDATE};
         }
         else {
-            my $sth_i = $dbh->prepare("INSERT INTO ranking(id, auto, admin, page_rank) VALUES ('$id', 0, 1000, $rank)");
+            my $sth_i = $dbh->prepare("INSERT INTO ranking(id, auto, admin, page_rank) VALUES ('$id', 0, 1000, $new_rank)");
             $sth_i->execute;
             INFO q{INSERT};
         }
-        INFO qq{$id, $rank};
+        INFO qq{$id, $new_rank, $num};
 
-        $rank++;
+        $rank = $rank + 10 + $total;
     }
 }
 
