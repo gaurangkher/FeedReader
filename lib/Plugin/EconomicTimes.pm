c5fd2ea2054cacd07d8e1840f71c4b4a410c6ede
gaurang <gaurangkher@yahoo.com>
2015-08-05 22:31:43 -0500

    add economic times

---
+++ lib/Plugin/EconomicTimes.pm
@@ -0,0 +1,75 @@
+package Plugin::EconomicTimes;
+
+use Data::Dumper;
+use Moose;
+use List::Flatten;
+use Mojo::DOM;
+use LWP::Simple;
+use HTML::HeadParser;
+use Story;
+use Log::Log4perl qw(:easy);
+
+with 'ParserRole';
+
+has feeds => (
+    is       => 'ro',
+    isa      => 'ArrayRef',
+    required => 1,
+    default  => sub {
+        [ 
+            'http://economictimes.indiatimes.com/rssfeedsdefault.cms'
+        ];
+    },
+);
+
+sub source_name {
+    return q{Economic Times};
+}
+
+sub parse {
+    my ( $self, $story ) = @_;
+
+    my $url = $story->{'link'};
+    INFO qq{$url};
+    my $pd    = $self->get_url($url);
+    my $args  = $self->parse_page($pd);
+    my $obj   = Story->new(
+        title       => $story->{title},
+        time        => $story->{'pubDate'},
+        source      => $self->source_name(),
+        url         => $url,
+        description => $story->{description},
+        %{$args},
+    );
+
+    return $obj;
+}
+
+sub parse_page {
+    my ( $self, $pd ) = @_;
+
+    my $page = Mojo::DOM->new($pd);
+
+    my $image_url = $page->find('meta[property="og:image"]')->first;
+    $image_url = $image_url->tree->[2]->{content};
+
+    my $author = $page->find('.byline')->first;
+    $author = $author->find('span')->first->all_text(0);
+
+    my $tags = $page->find('meta[name="keywords"]')->first;    
+    $tags = $tags->tree->[2]->{content};
+
+    my $content = $page->find('.Normal')->first->all_text(0);
+
+    my $category = $page->find('h2')->first->all_text(0);
+
+    return {
+        author      => $author,
+        content     => $content,
+        image_url   => $image_url,
+        tags        => $tags,
+        category    => $category,
+    };
+}
+
+1;
---
+++ t/plugin/economic_times.t
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../../lib";
+use Data::Dumper;
+use Test::More;
+use Test::MockObject;
+use File::Slurp;
+
+my $url = q{/home/gaurang/git/FeedReader/t/share/et};
+
+my $page = read_file($url);
+
+use_ok('Plugin::EconomicTimes');
+
+my $plugin = Plugin::EconomicTimes->new();
+
+my $story =  $plugin->parse_page($page);
+my $content = delete $story->{content};
+
+is_deeply(
+    $story,
+    {
+        'category' => 'News',
+        'author' => 'Nishanth Vasudevan',
+        'tags' => 'Tata Elxsi,stocks,Sensex,Retail investors,real estate,nifty,mutual funds,invest,Eicher Motors,Dalal Street,BSE,Britannia,Ashok Leyland',
+        'image_url' => 'http://economictimes.indiatimes.com/thumb/msid-48367348,width-600,resizemode-4/growth3_bccl.jpg'
+    }, 
+    q{got category, author, tags, image_url}
+);
+
+done_testing();
